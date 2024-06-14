6a1b8d699dc66e77718ebeeab7a1c79502deb689
Ludde <70239345+ludvikkristoffersen@users.noreply.github.com>
2024-06-14 14:06:43 +0200

    Added colors

--- fast-enum.sh
+++ fast-enum.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
+RED="\e[31m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
 echo "\n"
 
-echo "[*] OS/System Information"
-echo "OS: $(grep PRETTY_NAME /etc/os-release | cut -d '"' -f 2)"
-echo "OS Version: $(grep '^VERSION=' /etc/os-release | cut -d '"' -f 2)"
+echo "${RED}[*] OS / System Information${RESET}"
+echo "${YELLOW}OS:${RESET} $(grep PRETTY_NAME /etc/os-release | cut -d '"' -f 2)"
+echo "${YELLOW}OS Version:${RESET} $(grep '^VERSION=' /etc/os-release | cut -d '"' -f 2)"
 echo "Linux Kernel: $(dmesg | grep 'Linux version' | cut -d ' ' -f 8)"
 echo "Hostname: $(hostname)"
 
 echo "\n"
 
-echo "[*] Network Information"
+echo "${RED}[*] Network Information${RESET}"
 ip a | awk '/^[0-9]+:/ { iface=$2; sub(/:/, "", iface); } /^[[:space:]]*inet / { sub(/\/[0-9]+$/, "", $2); print "Interface:", iface, $2; }'
 echo "DNS Nameserver: $(cat /etc/resolv.conf | grep nameserver | cut -d ' ' -f 2)"
 
 echo "\n"
 
-echo "[*] Current User Information"
+echo "${RED}[*] Current User Information${RESET}"
 echo "Current User: $(whoami)"
 echo "ID: $(id | cut -d " " -f 1-2)"
 echo "Groups: $(groups)"
@@ -26,8 +30,8 @@ fi
 
 echo "\n"
 
-echo "[*] User Enumeration"
-echo "Listing users with shell!"
+echo "${RED}[*] User Enumeration${RESET}"
+echo "Listing all users with shell!\n"
 for user in $(awk -F: '$7 ~ /(\/bin\/bash|\/bin\/sh|\/bin\/ksh|\/bin\/zsh|\/usr\/bin\/fish)$/ {print $1}' /etc/passwd); do
 	groups=$(groups $user | cut -d ":" -f 2)
 	if [ "$(id -u)" -eq 0 ]; then
